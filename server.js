662baffbca3ff2df21da2a9defa659da881d38f8
Alexey Alan-Reys <alexey.alanreys@gmail.com>
2025-04-24 17:08:36 +1000

    Add dotenv and morgan for logging

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 import 'colors';
+import dotenv from 'dotenv';
 import express from 'express';
+import morgan from 'morgan';
 
 import authRoutes from './app/auth/auth.routes.js';
+
+dotenv.config();
 
 const app = express();
 
 async function main() {
+	if (process.env.NODE_ENV === 'development') {
+		app.use(morgan('dev'));
+	}
+
 	app.use(express.json());
 	app.use('/api/auth', authRoutes);
 
-	const PORT = 5000;
+	const PORT = process.env.PORT || 5000;
 
 	app.listen(
 		PORT,
