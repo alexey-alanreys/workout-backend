556215f0162ea904d134a76dae2edcbc8775d6d5
Alexey Alan-Reys <alexey.alanreys@gmail.com>
2025-04-26 13:12:05 +1000

    Implement exercise-log CRUD endpoints

--- app/exercise/exercise.routes.js
+++ app/exercise/exercise.routes.js
@@ -8,13 +8,23 @@ import {
 	getExercise,
 	updateExercise,
 } from './exercise.controller.js';
+import {
+	completeExerciseLog,
+	createNewExerciseLog,
+	getExerciseLog,
+	updateExerciseLogTime,
+} from './log/exercise-log.controller.js';
 
 const router = express.Router();
 
 router.route('/').get(protect, getExercise);
 router.route('/').post(protect, createNewExercise);
-
 router.route('/:id').put(protect, updateExercise);
 router.route('/:id').delete(protect, deleteExercise);
+
+router.route('/log/:id').get(protect, getExerciseLog);
+router.route('/log/:exerciseId').post(protect, createNewExerciseLog);
+router.route('/log/time/:id').put(protect, updateExerciseLogTime);
+router.route('/log/complete/:id').patch(protect, completeExerciseLog);
 
 export default router;
